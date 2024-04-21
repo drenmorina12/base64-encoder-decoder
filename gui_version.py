e3c1b146b443bc35af928270c051558456e47635
Dren Morina <drenmorina321@gmail.com>
2024-04-21 21:28:40 +0200

    Add input and otput text boxes

--- gui_version.py
+++ gui_version.py
@@ -4,4 +4,18 @@ from base64_encoder_decoder import encode, decode
 root = Tk()
 root.title("Base64 encoder/decoder")
 
+# Input
+input_label = Label(root, text="Input text")
+input_label.grid(row=0, column=0)
+
+input_text = Text(root, width=30, height=7, padx=5, pady=5)
+input_text.grid(row=1, column=0, padx=10, pady=10)
+
+# Output
+output_label = Label(root, text="Output text")
+output_label.grid(row=0, column=2)
+
+output_text = Text(root, width=30, height=7)
+output_text.grid(row=1, column=2, padx=10, pady=10)
+
 mainloop()
